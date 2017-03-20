ead15d492dd238a46ccd636ca2109668abc5824a
Alex Bogdanovsky <alex.bogdanovsky@gmail.com>
2017-03-20 11:22:58 +0200

    tic tac toe game with win conditions

--- ttc.js
+++ ttc.js
@@ -13,10 +13,54 @@ $(document).ready(function() {
             var index = Math.floor(Math.random() * emptyCols.length);
             $(emptyCols[index]).removeClass('empty').addClass('O')
             console.log('Computer have drawn a O');
+
         } else {
             console.log('space is taken');
             return;
         }
+
+            var modal=$('#myModal');
+
+        if($('#1').hasClass('X') && $('#2').hasClass('X') && $('#3').hasClass('X') ||
+            $('#4').hasClass('X') && $('#5').hasClass('X') && $('#6').hasClass('X') ||
+            $('#7').hasClass('X') && $('#8').hasClass('X') && $('#9').hasClass('X')){
+            console.log('X wins with a row');
+            modal.modal('show');
+            modal.one('hidden.bs.modal', function (e) {
+                console.log('game is clear to restart');
+                $('.col').removeClass('X').addClass('empty');
+                $('.col').removeClass('O').addClass('empty');
+                modal.off();
+        });
+        }
+
+        if($('#1').hasClass('X') && $('#4').hasClass('X') && $('#7').hasClass('X') ||
+            $('#2').hasClass('X') && $('#5').hasClass('X') && $('#8').hasClass('X') ||
+            $('#3').hasClass('X') && $('#6').hasClass('X') && $('#9').hasClass('X')){
+            console.log('X wins with a column');
+            modal.modal('show');
+            modal.one('hidden.bs.modal', function (e) {
+                console.log('game is clear to restart');
+                $('.col').removeClass('X').addClass('empty');
+                $('.col').removeClass('O').addClass('empty');
+                modal.off();
+            });
+        }
+
+        if($('#1').hasClass('X') && $('#5').hasClass('X') && $('#9').hasClass('X') ||
+            $('#3').hasClass('X') && $('#5').hasClass('X') && $('#7').hasClass('X')){
+            console.log('X wins with a diagnol');
+            modal.modal('show');
+            modal.one('hidden.bs.modal', function (e) {
+                console.log('game is clear to restart');
+                $('.col').removeClass('X').addClass('empty');
+                $('.col').removeClass('O').addClass('empty');
+                modal.off();
+            });
+        }
+
+
+
     });
 
 });
